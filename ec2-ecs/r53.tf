920a9674b09a394155996576b0daea1635d8418d
renancoradini <renancoradini@gmail.com>
2023-08-19 19:00:04 +0100

    25

--- ec2-ecs/r53.tf
+++ ec2-ecs/r53.tf
@@ -11,11 +11,11 @@ resource "aws_route53_record" "www" {
   }
 }
 
-resource "aws_route53_record" "hello_cert_dns" {
-  allow_overwrite = true
-  name            = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_type
-  zone_id         = var.r53_zone_id
-  ttl             = 60
-}
+# resource "aws_route53_record" "hello_cert_dns" {
+#   allow_overwrite = true
+#   name            = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_name
+#   records         = [tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_value]
+#   type            = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_type
+#   zone_id         = var.r53_zone_id
+#   ttl             = 60
+# }
