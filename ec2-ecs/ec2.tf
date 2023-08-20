9eb3884b2ab037afd84800b9a504c023ff801a2f
renancoradini <renancoradini@gmail.com>
2023-08-20 17:24:59 +0100

    comitao do dia 5

--- ec2-ecs/ec2.tf
+++ ec2-ecs/ec2.tf
@@ -23,8 +23,11 @@ resource "aws_launch_template" "tf_launch_template" {
   instance_type          = var.instance_type            #in variable file
   key_name               = var.key_name                 #in variable file
   user_data              = data.template_file.user_data.rendered
-  security_groups    = [aws_security_group.public.id]
-  iam_instance_profile   = aws_iam_instance_profile.ecs_agent.name
+  vpc_security_group_ids    = [aws_security_group.public.id]
+
+    iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_agent.name
+  }
 
   tag_specifications {
     resource_type = "instance"
