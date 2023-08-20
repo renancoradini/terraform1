ea10267f73f52026041bbd8e9df25a5411d32059
renancoradini <renancoradini@gmail.com>
2023-08-20 12:32:20 +0100

    commit2

--- ec2-ecs/alb.tf
+++ ec2-ecs/alb.tf
@@ -28,32 +28,32 @@ resource "aws_alb_target_group" "alb_public_webservice_target_group" {
 }
 ##### ALB - Listeners #####
 
-resource "aws_lb_listener" "lb_listener-webservice-https-redirect" {
+# resource "aws_lb_listener" "lb_listener-webservice-https-redirect" {
+#   load_balancer_arn = aws_lb.loadbalancer.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+#   default_action {
+#     type = "redirect"
+#     redirect {
+#       port        = "443"
+#       protocol    = "HTTPS"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
+
+resource "aws_lb_listener" "lb_listener-webservice-https" {
   load_balancer_arn = aws_lb.loadbalancer.arn
   port              = "80"
   protocol          = "HTTP"
+  port              = "80"
+  protocol          = "HTTP"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.ssl_certificate.arn
+
   default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.alb_public_webservice_target_group.id
   }
 }
-
-# resource "aws_lb_listener" "lb_listener-webservice-https" {
-  # load_balancer_arn = aws_lb.loadbalancer.arn
-  #port              = "443"
-  #protocol          = "HTTPS"
-  # port              = "80"
-  # protocol          = "HTTP"
-  #ssl_policy        = "ELBSecurityPolicy-2016-08"
-  #certificate_arn   = aws_acm_certificate.ssl_certificate.arn
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.alb_public_webservice_target_group.id
-#   }
-# }
 
